5fa5aea7b5ebf4226f35aeb18062c3af6fd2438c
Sens90 <blumeink@gmail.com>
2026-09-15 15:25:23 +0200

    Test active map and stats extraction

--- test_localized_stats.py
+++ test_localized_stats.py
@@ -9,7 +9,8 @@ from urllib.parse import urlsplit, urlunsplit
 source = ast.parse(Path(__file__).with_name('app.py').read_text())
 helpers = ast.Module(body=[node for node in source.body
     if isinstance(node, ast.FunctionDef) and node.name in
-    {'italian_planet_url', 'telegram_text_chunks'}], type_ignores=[])
+    {'italian_planet_url', 'telegram_text_chunks', 'brawlplanet_map_urls',
+     'brawlplanet_rotation_manifest'}], type_ignores=[])
 scope = {'re': re, 'urlsplit': urlsplit, 'urlunsplit': urlunsplit}
 exec(compile(helpers, 'app.py', 'exec'), scope)
 
@@ -34,6 +35,25 @@ class LocalizedStatsTests(unittest.TestCase):
         split = scope['telegram_text_chunks']
         self.assertEqual(list(split('')), [])
         self.assertEqual(''.join(split('x' * 9000)), 'x' * 9000)
+
+    def test_map_links_and_manifest(self):
+        result = {
+            'url': 'https://www.brawlplanet.com/it/maps',
+            'raw_content': (
+                'https://www.brawlplanet.com/it/maps/grassknot_brawlball\n'
+                'https://www.brawlplanet.com/it/maps/hotpotato_heist'
+            )
+        }
+        urls = scope['brawlplanet_map_urls']([result])
+        self.assertEqual(len(urls), 2)
+        pages = [
+            {'url': urls[0], 'title': 'Migliori Brawler per Campetto in erba - Footbrawl'},
+            {'url': urls[1], 'raw_content': '# Battigia ustionante\n## Rapina'},
+        ]
+        manifest = scope['brawlplanet_rotation_manifest'](pages)
+        self.assertEqual([(row['map'], row['mode']) for row in manifest], [
+            ('Campetto in erba', 'Footbrawl'), ('Battigia ustionante', 'Rapina')
+        ])
 
 
 if __name__ == '__main__':
